1dea52d6aa43c908843b775c675dba4dd489bab2
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2021-02-04 22:23:55 -0800

    naive implementation of a cache using async storage

--- src/api/Search.tsx
+++ src/api/Search.tsx
@@ -1,10 +1,33 @@
 import { adapt, APIDrink, Drink } from './Adapter'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
-export const fetchSearch = (search: string): Promise<Array<Drink>> => {
+const save = (key: string, drinks: Array<Drink>) => {
+  if (drinks.length) {
+    setTimeout(async () => {
+      try {
+        AsyncStorage.setItem(key, JSON.stringify(drinks))
+      } catch (e) {
+        // saving error
+      }
+    })
+  }
+}
+
+export const fetchSearch = async (search: string): Promise<Array<Drink>> => {
+  const cachedDrinks = await AsyncStorage.getItem(search)
+
+  if (cachedDrinks) {
+    return JSON.parse(cachedDrinks)
+  }
+
   return fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`,
   )
     .then((r) => r.json())
     .then((result) => result.drinks || [])
     .then((result) => result.map((d: APIDrink) => adapt(d)))
+    .then((drinks) => {
+      save(search, drinks)
+      return drinks
+    })
 }
