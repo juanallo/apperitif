d8f0ec69f132e2a6046b939cd59cdfe39f4600cf
Juan Allo Ron <2070972+juanallo@users.noreply.github.com>
2021-02-04 22:27:19 -0800

    adding a timestamp to the cache to simplify cleanup in the future

--- src/api/Search.tsx
+++ src/api/Search.tsx
@@ -1,11 +1,18 @@
 import { adapt, APIDrink, Drink } from './Adapter'
 import AsyncStorage from '@react-native-async-storage/async-storage'
 
+//TODO think about how to clean the cache.
 const save = (key: string, drinks: Array<Drink>) => {
   if (drinks.length) {
     setTimeout(async () => {
       try {
-        AsyncStorage.setItem(key, JSON.stringify(drinks))
+        AsyncStorage.setItem(
+          key,
+          JSON.stringify({
+            timestamp: Date.now(),
+            data: drinks,
+          }),
+        )
       } catch (e) {
         // saving error
       }
@@ -17,7 +24,7 @@ export const fetchSearch = async (search: string): Promise<Array<Drink>> => {
   const cachedDrinks = await AsyncStorage.getItem(search)
 
   if (cachedDrinks) {
-    return JSON.parse(cachedDrinks)
+    return JSON.parse(cachedDrinks).data
   }
 
   return fetch(
